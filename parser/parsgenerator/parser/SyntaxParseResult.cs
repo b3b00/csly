2280304103311e1d6e297c4ecbbdecbb52578056
olivier <olivier.duhart@gmail.com>
2017-04-17 19:58:21 +0200

    initial

---
+++ parser/parsgenerator/parser/SyntaxParseResult.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using parser.parsergenerator.syntax;
+
+namespace parser.parsergenerator.parser
+{
+    public class SyntaxParseResult<T> {
+        public ConcreteSyntaxNode<T> Root {get; set;}
+
+        public bool IsError{get; set;}
+
+        public List<string> Errors {get; set;}
+
+    }
+}
