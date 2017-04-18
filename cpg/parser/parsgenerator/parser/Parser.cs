6d0b5eb13feec11b9a6c1e020d972435c5e8f2ca
Olivier Duhart <olivier.duhart@sage.com>
2017-04-18 12:14:13 +0200

    LL parser

---
+++ cpg/parser/parsgenerator/parser/Parser.cs
@@ -0,0 +1,29 @@
+﻿using parser.parsergenerator.generator;
+using parser.parsergenerator.parser;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cpg.parser.parsgenerator.parser
+{
+    public class Parser<T>
+    {
+        public ISyntaxParser<T> SyntaxParser { get; set; }
+        public ConcreteSyntaxTreeVisitor<T> Visitor { get; set; }
+        public Parser(ISyntaxParser<T> syntaxParser, ConcreteSyntaxTreeVisitor<T> visitor)
+        {
+            SyntaxParser = syntaxParser;
+            Visitor = visitor;
+        }
+        public object Parse(IList<T> tokens)
+        {
+            object result = null;
+            SyntaxParseResult<T> syntaxResult =  SyntaxParser.Parse(tokens);
+            if (!syntaxResult.IsError && syntaxResult.Root != null)
+            {
+                result = Visitor.VisitSyntaxTree(syntaxResult.Root);
+            }
+            return result;
+        }
+    }
+}
---
+++ cpg/parser/parsgenerator/parser/llparser/LLSyntaxParser.cs
@@ -0,0 +1,26 @@
+﻿using parser.parsergenerator.generator;
+using parser.parsergenerator.parser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cpg.parser.parsgenerator.parser.llparser
+{
+    public class LLSyntaxParser<T> : ISyntaxParser<T>
+    {
+        public ParserConfiguration<T> Configuration { get; set; }
+
+        public LLSyntaxParser(ParserConfiguration<T> configuration, string startingNonTerminal)
+        {
+            Configuration = configuration;
+        }
+
+        public SyntaxParseResult<T> Parse(IList<T> tokens)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
