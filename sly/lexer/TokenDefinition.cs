e36b0280432cb4a83e0c96b9fce54a6b2b159326
Olivier Duhart <olivier.duhart@sage.com>
2017-06-07 14:40:22 +0200

    bugfix #9 column and line number on error reporting

--- sly/lexer/TokenDefinition.cs
+++ sly/lexer/TokenDefinition.cs
@@ -3,14 +3,24 @@ using System.Text.RegularExpressions;
 
 namespace sly.lexer
 {
+    /// <summary>
+    /// defines a token assiating :
+    ///    - a token identifier (which type is T)
+    ///    - and a regular expression capturing the token
+    /// a token may be skipped and / or match an end of line   
+    ///
+    /// </summary>
+    /// <typeparam name="T">T is the enum Token type</typeparam>
     public class TokenDefinition<T>
     {
-        
-        public TokenDefinition(T token, string regex)
-            : this(token, regex, false,false)
-        {
-        }
-
+       
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="token"> the token ID</param>
+        /// <param name="regex"> the regular expression for the token</param>
+        /// <param name="isIgnored">true if the token must ignored (i.e the lexer does not return it, used for whitespaces for instance) </param>
+        /// <param name="isEndOfLine">true if the token matches an end of line (for line counting)</param>
         public TokenDefinition(T token, string regex,bool isIgnored= false,bool isEndOfLine = false)
         {
             TokenID = token;
