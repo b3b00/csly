cdec97ec67457a88dbc2714b8d056df6c88d116f
Olivier Duhart <olivier.duhart@sage.com>
2017-11-10 19:31:38 +0100

    json parser using generic lexer

--- samples/jsonparser/EBNFJSONParser.cs
+++ samples/jsonparser/EBNFJSONParser.cs
@@ -198,4 +198,176 @@ namespace jsonparser
 
 
     }
+
+    public class EbnfJsonGenericParser
+    {
+
+
+
+
+
+
+        #region root
+
+        [Production("root : value")]
+        public JSon Root(JSon value)
+        {
+            return value;
+        }
+
+
+        #endregion
+
+        #region VALUE
+
+        [Production("value : STRING")]
+        public JSon StringValue(Token<JsonTokenGeneric> stringToken)
+        {
+            return new JValue(stringToken.StringWithoutQuotes);
+        }
+
+        [Production("value : INT")]
+        public object IntValue(Token<JsonTokenGeneric> intToken)
+        {
+            return new JValue(intToken.IntValue);
+        }
+
+        [Production("value : DOUBLE")]
+        public object DoubleValue(Token<JsonTokenGeneric> doubleToken)
+        {
+            double dbl = double.MinValue;
+            try
+            {
+                string[] doubleParts = doubleToken.Value.Split('.');
+                dbl = double.Parse(doubleParts[0]);
+                if (doubleParts.Length > 1)
+                {
+                    double decimalPart = double.Parse(doubleParts[1]);
+                    for (int i = 0; i < doubleParts[1].Length; i++)
+                    {
+                        decimalPart = decimalPart / 10.0;
+                    }
+                    dbl += decimalPart;
+                }
+            }
+            catch (Exception)
+            {
+                dbl = double.MinValue;
+            }
+
+            return new JValue(dbl);
+        }
+
+        [Production("value : BOOLEAN")]
+        public object BooleanValue(Token<JsonTokenGeneric> boolToken)
+        {
+            return new JValue(bool.Parse(boolToken.Value));
+        }
+
+        [Production("value : NULL")]
+        public object NullValue(object forget)
+        {
+            return new JNull();
+        }
+
+        [Production("value : object")]
+        public JSon ObjectValue(JSon value)
+        {
+            return value;
+        }
+
+        [Production("value: list")]
+        public JSon ListValue(JList list)
+        {
+            return list;
+        }
+
+        #endregion
+
+        #region OBJECT
+
+        [Production("object: ACCG ACCD")]
+        public JSon EmptyObjectValue(object accg, object accd)
+        {
+            return new JObject();
+        }
+
+        [Production("object: ACCG members ACCD")]
+        public JSon AttributesObjectValue(object accg, JObject members, object accd)
+        {
+            return members;
+        }
+
+
+        #endregion
+        #region LIST
+
+        [Production("list: CROG CROD")]
+        public JSon EmptyList(object crog, object crod)
+        {
+            return new JList();
+        }
+
+        [Production("list: CROG listElements CROD")]
+        public JSon List(object crog, JList elements, object crod)
+        {
+            return elements;
+        }
+
+
+        [Production("listElements: value additionalValue+")]
+        public JSon listElements(JSon head, List<JSon> tail)
+        {
+            JList values = new JList(head);
+            values.AddRange(tail);
+            return values;
+        }
+
+        [Production("additionalValue: COMMA value")]
+        public JSon ListElementsOne(Token<JsonTokenGeneric> discardedComma, JSon value)
+        {
+            return value;
+        }
+
+
+
+        #endregion
+
+        #region PROPERTIES
+
+
+
+        [Production("members: property additionalProperty+")]
+        public object Members(JObject head, List<JSon> tail)
+        {
+            JObject value = new JObject();
+            value.Merge(head);
+            foreach (JSon p in tail)
+            {
+                value.Merge((JObject)p);
+            }
+            return value;
+        }
+
+        [Production("additionalProperty : COMMA property")]
+        public object property(Token<JsonTokenGeneric> comma, JObject property)
+        {
+            return property;
+        }
+
+        [Production("property: STRING COLON value")]
+        public object property(Token<JsonTokenGeneric> key, object colon, JSon value)
+        {
+            return new JObject(key.StringWithoutQuotes, value);
+        }
+
+
+        
+
+        #endregion
+
+
+
+
+    }
 }
--- samples/jsonparser/JsonToken.cs
+++ samples/jsonparser/JsonToken.cs
@@ -35,4 +35,30 @@ namespace jsonparser
         NULL = 14,        
     }
 
+    public enum JsonTokenGeneric
+    {
+        [Lexeme(GenericToken.String)]
+        STRING = 1,
+        [Lexeme(GenericToken.Double)]
+        DOUBLE = 2,
+        [Lexeme(GenericToken.Int)]
+        INT = 3,
+        [Lexeme(GenericToken.KeyWord,"true","false")]
+        BOOLEAN = 4,
+        [Lexeme(GenericToken.SugarToken,"{")]
+        ACCG = 5,
+        [Lexeme(GenericToken.SugarToken, "}")]
+        ACCD = 6,
+        [Lexeme(GenericToken.SugarToken, "[")]
+        CROG = 7,
+        [Lexeme(GenericToken.SugarToken, "]")]
+        CROD = 8,
+        [Lexeme(GenericToken.SugarToken, ",")]
+        COMMA = 9,
+        [Lexeme(GenericToken.SugarToken, ":")]
+        COLON = 10,        
+        [Lexeme(GenericToken.KeyWord, "null")]
+        NULL = 14,
+    }
+
 }
