d989b7c0c357f800d0da1f8e9fe6388ece5aaf84
b3b00 <olivier.duhart@gmail.com>
2018-12-07 13:53:17 +0100

    #91 initial commit

---
+++ samples/GenericLexerWithCallbacks/TestCallbacks.cs
@@ -0,0 +1,15 @@
+﻿using sly.lexer;
+
+namespace GenericLexerWithCallbacks
+{
+    public class TestCallbacks
+    {
+
+        [TokenCallback((int)Tokens.IDENTIFIER)]
+        public Token<Tokens> TranslateID(Token<Tokens> token)
+        {
+            return token;
+        } 
+        
+    }
+}
---
+++ samples/GenericLexerWithCallbacks/TokenCallbackAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GenericLexerWithCallbacks
+{
+    public class TokenCallbackAttribute : Attribute
+    {
+        public int EnumValue { get; set;}
+        
+        public TokenCallbackAttribute(int enumValue)
+        {
+            EnumValue = enumValue;
+        }
+        
+    }
+}
