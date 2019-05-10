0e090ea9dba70c53a6769f3144cc1776816a608f
Alexandre Marcondes <alexandre.marcondes@gmail.com>
2019-05-11 01:14:15 +0200

    * Creating SingleLineCommentAttribute and MultiLineCommentAttribute * Creating new tests for the commenting subsystem

---
+++ sly/lexer/MultiLineCommentAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sly.lexer
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+    public class MultiLineCommentAttribute : CommentAttribute
+    {
+        public string MultiLineCommentEnd;
+
+        public string MultiLineCommentStart;
+
+        private new string SingleLineCommentStart;
+
+        public MultiLineCommentAttribute(string start, string end) : base(null, start, end)
+        { }
+    }
+}
---
+++ sly/lexer/SingleLineCommentAttribute.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace sly.lexer
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
+    public class SingleLineCommentAttribute : CommentAttribute
+    {
+        private new string MultiLineCommentEnd;
+
+        private new string MultiLineCommentStart;
+
+        public string SingleLineCommentStart;
+
+        public SingleLineCommentAttribute(string start) : base(start, null, null)
+        { }
+    }
+}
