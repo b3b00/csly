68b7cdb58dab2a8f1ab8fb6d8d3d071ed842e88c
b3b00 <olivier.duhart@gmail.com>
2019-05-13 17:53:16 +0200

    code cleaning

--- sly/lexer/MultiLineCommentAttribute.cs
+++ sly/lexer/MultiLineCommentAttribute.cs
@@ -5,12 +5,6 @@ namespace sly.lexer
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
     public class MultiLineCommentAttribute : CommentAttribute
     {
-        public string MultiLineCommentEnd;
-
-        public string MultiLineCommentStart;
-
-        private new string SingleLineCommentStart;
-
         public MultiLineCommentAttribute(string start, string end) : base(null, start, end)
         { }
     }
--- sly/lexer/SingleLineCommentAttribute.cs
+++ sly/lexer/SingleLineCommentAttribute.cs
@@ -5,12 +5,6 @@ namespace sly.lexer
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = true)]
     public class SingleLineCommentAttribute : CommentAttribute
     {
-        private new string MultiLineCommentEnd;
-
-        private new string MultiLineCommentStart;
-
-        public string SingleLineCommentStart;
-
         public SingleLineCommentAttribute(string start) : base(start, null, null)
         { }
     }
