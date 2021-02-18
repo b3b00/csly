b43478da0b4554ab975096d8acb0053007a5ef34
Mauricio Morales <mauricio.morales@careerbuilder.com>
2021-02-18 11:26:18 -0600

    Issue #223 Early EOS before ) TERM

---
+++ ParserTests/Issue223_EarlyEos/EarlyEosTests.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Linq;
+using sly.parser.generator;
+using Xunit;
+
+namespace ParserTests.Issue223_EarlyEos
+{
+    public class EarlyEosTests
+    {
+        [Fact]
+        public void Early_EOS_NRE()
+        {
+            var queryExpression = Parse("( java AND dotnet )");
+            Assert.IsType<GroupExpression>(queryExpression);
+            Assert.Equal("(java AND dotnet)", queryExpression.ToString());
+        }
+
+        private static Expression Parse(string query)
+        {
+            var parserInstance = new EarlyEosParser();
+            var builder = new ParserBuilder<EarlyEosToken, Expression>();
+
+            var buildResult =
+                builder.BuildParser(parserInstance, ParserType.EBNF_LL_RECURSIVE_DESCENT, "expression");
+            if (buildResult.IsError)
+                throw new AggregateException(
+                    buildResult.Errors
+                        .Select(e => new Exception($"{e.Level} {e.Code} {e.Message}"))
+                );
+            var parser = buildResult.Result;
+
+            var queryExpression = parser.Parse(query.Trim());
+            if (queryExpression.IsError)
+                throw new AggregateException(
+                    queryExpression.Errors
+                        .Select(e => new Exception(e.ErrorMessage))
+                );
+
+            return queryExpression.Result;
+        }
+    }
+}
---
+++ ParserTests/Issue223_EarlyEos/Expression.cs
@@ -0,0 +1,7 @@
+namespace ParserTests.Issue223_EarlyEos
+{
+    public abstract class Expression
+    {
+        public abstract string ToQuery();
+    }
+}
