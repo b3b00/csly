34fdabe163c042c44d72d7b69f9ab00d2ae490e1
b3b00 <olivier.duhart@gmail.com>
2021-11-07 19:01:19 +0100

    .

--- Issue254/Program.cs
+++ Issue254/Program.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
 using BravoLights.Ast;
 using BravoLights.Common.Ast;
@@ -9,17 +11,40 @@ namespace Issue254
     public static class Program
     {
 
+        
+        
         public static void Main(string[] args)
         {
+            var tests = new Dictionary<string, double>()
+            {
+                { "1 + 2 * 3", 7.0 },
+                { "2 - 3 / 4", 1.25 },
+                { "-3 - -4", 1.0 },
+                { "-3--4", 1.0 },
+                { "-3+-4", -7.0 },
+                { "-(1+2)", -3.0 },
+                { "-(1+2 * 3)", -7.0 },
+                { "3 * -2", -6.0 }
+            };
+            
             var c = new Stopwatch();
-            c.Start();
+            
             var parserInstance = new MSFSExpressionParser();
             var builder = new ParserBuilder<ExpressionToken, IAstNode>();
             var parser = builder.BuildParser(parserInstance, ParserType.EBNF_LL_RECURSIVE_DESCENT, "logicalExpression");
 
-            var x = parser.Result.Parse("-(1+2 * 3)");
-            c.Stop();
-            Console.WriteLine(c.ElapsedMilliseconds+" ms");
+            foreach (var test in tests)
+            {
+                c.Reset();
+                c.Start();
+                var x = parser.Result.Parse(test.Key);
+                bool isok = x.IsOk;
+                c.Stop();
+                Console.WriteLine($"test {test.Key} : {isok} : {c.ElapsedMilliseconds} ms");
+            }
+
+            ;
+
         }
     }
 }
