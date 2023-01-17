ae9a1aba8f2c05bf1ea472becf0d572805a6dd45
b3b00 <olivier.duhart@gmail.com>
2023-01-17 13:32:30 +0100

    short character token

---
+++ sly/lexer/attributes/CharAttribute.cs
@@ -0,0 +1,9 @@
+namespace sly.lexer
+{
+    public class CharacterAttribute : LexemeAttribute
+    {
+        public CharacterAttribute(string delimiter = "\"", string escape = "\\") : base(GenericToken.String, delimiter, escape)
+        {   
+        } 
+    }
+}
