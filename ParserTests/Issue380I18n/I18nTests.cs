f76ff319c0c2c330d441ad3d034e00c144752421
b3b00 <olivier.duhart@gmail.com>
2023-08-16 09:51:57 +0200

    i18n unit test

--- ParserTests/Issue380I18n/I18nTests.cs
+++ ParserTests/Issue380I18n/I18nTests.cs
@@ -10,11 +10,11 @@ namespace ParserTests.Issue380I18n;
 
 public class I18nTests
 {
-    private static Parser<ExpressionToken, double> BuildParser()
+    private static Parser<ExpressionToken, double> BuildParser(string i18n)
     {   
         var StartingRule = $"{nameof(SimpleExpressionParser)}_expressions";
         var parserInstance = new SimpleExpressionParser();
-        var builder = new ParserBuilder<ExpressionToken, double>();
+        var builder = new ParserBuilder<ExpressionToken, double>(i18n);
         var pBuild = builder.BuildParser(parserInstance, ParserType.EBNF_LL_RECURSIVE_DESCENT, StartingRule);
         Check.That(pBuild).IsOk();
         return pBuild.Result;
@@ -23,7 +23,7 @@ public class I18nTests
     [Fact]
     public static void TestErrorMessage()
     {
-        var parser = BuildParser();
+        var parser = BuildParser("en");
         var result = parser.Parse("2 ( 2");
         Check.That(result).Not.IsOkParsing();
         var errors = result.Errors;
@@ -35,10 +35,14 @@ public class I18nTests
         Check.That(unexpectedTokenError.ExpectedTokens).IsNotNull();
         Check.That(unexpectedTokenError.ExpectedTokens).Not.IsEmpty();
         Check.That(unexpectedTokenError.ExpectedTokens).CountIs(5);
+        Check.That(unexpectedTokenError.UnexpectedToken.Position.Line).IsEqualTo(1);
+        Check.That(unexpectedTokenError.UnexpectedToken.Position.Column).IsEqualTo(3);
         Check.That(unexpectedTokenError.ExpectedTokens.Extracting(x => x.TokenId)).Contains(new[]
         {
             ExpressionToken.FACTORIAL,ExpressionToken.DIVIDE,ExpressionToken.TIMES,ExpressionToken.MINUS,ExpressionToken.PLUS
         });
-        // TODO : check message
+        Check.That(error.ErrorMessage).Contains("(line 1, column 3)");
+        Check.That(error.ErrorMessage).Contains("opening parenthesis");
+
     }
 }
