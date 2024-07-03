1c582bf251b63afd3cfee1507626822ae99acf48
b3b00 <olivier.duhart@gmail.com>
2024-07-03 16:43:46 +0200

    unit tests

--- tests/ParserTests/NodeNamesTests.cs
+++ tests/ParserTests/NodeNamesTests.cs
@@ -12,7 +12,7 @@ namespace ParserTests;
 public class NodeNamesTests
 {
     [Fact]
-    public void TestNodeNames()
+    public void TestBNFNodeNames()
     {
         var parserInstance = new ExpressionParser();
         var builder = new ParserBuilder<ExpressionToken, int>();
@@ -28,4 +28,24 @@ public class NodeNamesTests
         Check.That(root.Children[0].Name).IsEqualTo("term");
         Check.That(root.Children[2].Name).IsEqualTo("expression");
     }
+    
+    [Fact]
+    public void TestEBNFNodeNames()
+    {
+        var parserInstance = new ExpressionParser();
+        var builder = new ParserBuilder<ExpressionToken, int>();
+        var parser = builder.BuildParser(parserInstance, ParserType.EBNF_LL_RECURSIVE_DESCENT, "expression").Result;
+        var r = parser.Parse("1+1");
+        Check.That(r).IsOkParsing();
+        Check.That(r.SyntaxTree).IsNotNull();
+        Check.That(r.SyntaxTree.Name).IsEqualTo("addOrSubstract");
+        Check.That(r.SyntaxTree).IsInstanceOf<SyntaxNode<ExpressionToken>>();
+        var root = r.SyntaxTree as SyntaxNode<ExpressionToken>;
+        Check.That(r).IsNotNull();
+        Check.That(root.Children).CountIs(3);
+        Check.That(root.Children[0].Name).IsEqualTo("term");
+        Check.That(root.Children[2].Name).IsEqualTo("expression");
+    }
+    
+    
 }
