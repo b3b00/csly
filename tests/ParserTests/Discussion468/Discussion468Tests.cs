71145ac40269f775489dfd66a57d0fa7b78f7605
b3b00 <olivier.duhart@gmail.com>
2024-08-01 09:04:16 +0200

    discussion #468 : the generic lexer extension way

--- tests/ParserTests/Discussion468/Discussion468Tests.cs
+++ tests/ParserTests/Discussion468/Discussion468Tests.cs
@@ -25,7 +25,7 @@ namespace ParserTests.Discussion468
                 Check.That(tokens).IsNotNull();
                 Check.That(tokens).CountIs(2);
                 var idToken = tokens[0];
-                Check.That(idToken.TokenID).IsEqualTo(Discussion468Lexer.INDENTIFIER);
+                Check.That(idToken.TokenID).IsEqualTo(Discussion468Lexer.IDENTIFIER);
                 Check.That(idToken.Value).IsEqualTo(source);
             }
             else
@@ -43,6 +43,41 @@ namespace ParserTests.Discussion468
             Check.That(lexer).IsNotNull();
             CheckId(lexer,"_:word1234",true);
             CheckId(lexer,"word_1234.otherword",true);
+            CheckId(lexer,"1_:word1234",false);
+            
+        }
+        
+        private static void CheckId(ILexer<Discussion468WithExtensionLexer> lexer,string source, bool isOk)
+        {
+            
+            var lexingResult = lexer.Tokenize(source);
+            if (isOk)
+            {
+                Check.That(lexingResult).IsOkLexing();
+                var tokens = lexingResult.Tokens;
+                Check.That(tokens).IsNotNull();
+                Check.That(tokens).CountIs(2);
+                var idToken = tokens[0];
+                Check.That(idToken.TokenID).IsEqualTo(Discussion468WithExtensionLexer.IDENTIFIER);
+                Check.That(idToken.Value).IsEqualTo(source);
+            }
+            else
+            {
+                Check.That(lexingResult).Not.IsOkLexing();
+            }
+        }
+        
+        [Fact]
+        public static void Test468WithExtension()
+        {
+            var buildResult = LexerBuilder.BuildLexer<Discussion468WithExtensionLexer>(extensionBuilder:Discussion468Extension.AddExtension);
+            Check.That(buildResult).IsOk();
+            var lexer = buildResult.Result;
+            Check.That(lexer).IsNotNull();
+            CheckId(lexer,"_:word1234",true);
+            CheckId(lexer,"word_1234.otherword",true);
+            CheckId(lexer,"Test1",true);
+            CheckId(lexer,"Test12",true);
             CheckId(lexer,"1_:word1234",false);
             
         }
