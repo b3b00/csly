258c6aefb9e2ba6773692f1cf419d9f83c20473e
b3b00 <olivier.duhart@gmail.com>
2024-08-08 15:49:32 +0200

    parser build : indented while unit test working

--- src/aot/Program.cs
+++ src/aot/Program.cs
@@ -2,62 +2,90 @@
 
 using aot.lexer;
 using aot.parser;
+using ParserTests.aot;
 
 
 // testing lexer builder 
+//
+// var builder = new TestAotLexerBuilder();
+// var lexerBuilder = builder.FluentInitializeCenericLexerForLexerTest();
+// if (lexerBuilder != null)
+// {
+//     var lexer = lexerBuilder.Build();
+//     string source = "2 + 2 * ( 3 / 8) PLUS 42.42 100!";
+//     Console.WriteLine($"tokenize >{source}<");
+//     if (lexer.IsError)
+//     {
+//         foreach (var error in lexer.Errors)
+//         {
+//             Console.WriteLine(error);
+//         }
+//
+//         return;
+//     }
+//     var lexingResult = lexer.Result.Tokenize(source);
+//     if (lexingResult.IsOk)
+//     {
+//         Console.WriteLine("lexing OK");
+//         foreach (var token in lexingResult.Tokens)
+//         {
+//             Console.WriteLine(token.ToString());
+//         }
+//     }
+//     else
+//     {
+//         Console.WriteLine($"lexing KO : {lexingResult.Error}");
+//     }
+// }
+//
+// // testing parser builder
+// var pBuilder = new TestAotParserBuilder();
+// var b = pBuilder.FluentInitializeCenericLexer();
+// if (b.IsError)
+// {
+//     foreach (var error in b.Errors)
+//     {
+//         Console.WriteLine(error);
+//     }
+//
+//     return;
+// }
+// var r = b.Result.Parse("2 + 2 * 2");
+// if (r.IsOk)
+// {
+//     Console.WriteLine($"parse OK : {r.Result}");
+// }
+// else
+// {
+//     foreach (var error in r.Errors)
+//     {
+//         Console.WriteLine(error.ErrorMessage);
+//     }
+// }
 
-var builder = new TestAotLexerBuilder();
-var lexerBuilder = builder.FluentInitializeCenericLexerForLexerTest();
-if (lexerBuilder != null)
+AotIndentedWhileParserBuilder builder = new AotIndentedWhileParserBuilder();
+
+var b = builder.BuildAotWhileParser();
+var p = b.BuildParser();
+if (p.IsOk)
 {
-    var lexer = lexerBuilder.Build();
-    string source = "2 + 2 * ( 3 / 8) PLUS 42.42 100!";
-    Console.WriteLine($"tokenize >{source}<");
-    if (lexer.IsError)
+    Console.WriteLine(p.Result.Configuration.Dump());
+    string program = @"
+a:=0
+while a < 10 do 
+	print a
+	a := a +1
+";
+    var r = p.Result.Parse(program);
+    if (r.IsOk)
     {
-        foreach (var error in lexer.Errors)
-        {
-            Console.WriteLine(error);
-        }
-
-        return;
-    }
-    var lexingResult = lexer.Result.Tokenize(source);
-    if (lexingResult.IsOk)
-    {
-        Console.WriteLine("lexing OK");
-        foreach (var token in lexingResult.Tokens)
-        {
-            Console.WriteLine(token.ToString());
-        }
+        Console.WriteLine("OK");
     }
     else
     {
-        Console.WriteLine($"lexing KO : {lexingResult.Error}");
-    }
-}
-
-// testing parser builder
-var pBuilder = new TestAotParserBuilder();
-var b = pBuilder.FluentInitializeCenericLexer();
-if (b.IsError)
-{
-    foreach (var error in b.Errors)
-    {
-        Console.WriteLine(error);
-    }
-
-    return;
-}
-var r = b.Result.Parse("2 + 2 * 2");
-if (r.IsOk)
-{
-    Console.WriteLine($"parse OK : {r.Result}");
-}
-else
-{
-    foreach (var error in r.Errors)
-    {
-        Console.WriteLine(error.ErrorMessage);
+        foreach (var error in r.Errors)
+        {
+            Console.WriteLine(error.ErrorMessage);
+        }
     }
 }
