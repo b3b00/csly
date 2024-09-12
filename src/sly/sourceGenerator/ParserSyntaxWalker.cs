dbdb65ba9cc56343ea29222723f580ab26bdf6e0
b3b00 <olivier.duhart@gmail.com>
2024-09-12 16:15:58 +0200

    taming complexity

--- src/sly/sourceGenerator/ParserSyntaxWalker.cs
+++ src/sly/sourceGenerator/ParserSyntaxWalker.cs
@@ -85,110 +85,40 @@ public class ParserSyntaxWalker : CslySyntaxWalker
             .ToList();
         var nodeName = nodeNames.Any() ? nodeNames[0].ArgumentList.Arguments[0].Expression.ToString() : null;
 
-        if (!string.IsNullOrEmpty(nodeName))
-        {
-            ;
-        }
-        
         foreach (var attribute in attributes)
         {
             switch (attribute.Name.ToString())
             {
-                case "Production":
+                case "Production" when IsOperand(node):
                 {
-                    var rule = GetAttributeArgs(attribute,withLeadingComma:false);
-                    if (IsOperand(node))
-                    {
-                        _builder.AppendLine($".Operand({rule},");
-                        AddProductionVisitor(node);
-                        _builder.AppendLine(")");
-                    }
-                    else
-                    {
-                        _builder.AppendLine($".Production({rule},");
-                        AddProductionVisitor(node);
-                        _builder.AppendLine(")");
-                    }
-                    break;
-                }
-                case "Operation":
-                {
-                    if (attribute.ArgumentList.Arguments[0].Expression is CastExpressionSyntax cast)
-                    {
-                        var realArg = (attribute.ArgumentList.Arguments[0].Expression as CastExpressionSyntax).Expression.ToString();
-                        _builder.AppendLine(
-                            $".Operation({realArg} {GetAttributeArgs(attribute, skip:1)},");
-                        AddProductionVisitor(node);
-                        _builder.AppendLine(")");
-                    }
-                    else
-                    {
-                        _builder.AppendLine(
-                            $".Operation({GetAttributeArgs(attribute, withLeadingComma: false)},");
-                        AddProductionVisitor(node);
-                        _builder.AppendLine(")");
-                    }
-
-                    break;
-                }
-                case "Prefix":
-                {
-                    var precedence = attribute.ArgumentList.Arguments[2].Expression.ToString();
-                    if (attribute.ArgumentList.Arguments[0].Expression is CastExpressionSyntax cast)
-                    {
-                        var realArg = (attribute.ArgumentList.Arguments[0].Expression as CastExpressionSyntax).Expression.ToString();
-                        _builder.AppendLine($".Prefix({realArg}, {precedence}, ");
-                        AddProductionVisitor(node);
-                        _builder.AppendLine(")");
-                    }
-                    else
-                    {
-                        _builder.AppendLine(
-                            $".Prefix({attribute.ArgumentList.Arguments[0].Expression.ToString()}, {precedence},");
-                        AddProductionVisitor(node);
-                        _builder.AppendLine(")");
-                    }
-
-                    break;
-                }
-                case "Postfix":
-                {
-                    var precedence = attribute.ArgumentList.Arguments[2].Expression.ToString();
-                    
-                    if (attribute.ArgumentList.Arguments[0].Expression is CastExpressionSyntax cast)
-                    {
-                        var realArg = (attribute.ArgumentList.Arguments[0].Expression as CastExpressionSyntax).Expression.ToString();
-                        _builder.AppendLine(
-                            $".Postfix({realArg}, {precedence},");
-                        AddProductionVisitor(node);
-                        _builder.AppendLine(")");
-                    }
-                    else
-                    {
-                        _builder.AppendLine(
-                            $".Postfix({attribute.ArgumentList.Arguments[0].Expression.ToString()}, {precedence},");
-                        AddProductionVisitor(node);
-                        _builder.AppendLine(")");
-                    }
-
-                    break;
-                }
-                case "Infix":
-                {
-                    if (attribute.ArgumentList.Arguments[0].Expression is CastExpressionSyntax cast)
-                    {
-                        var realArg = (attribute.ArgumentList.Arguments[0].Expression as CastExpressionSyntax).Expression.ToString();
-                        _builder.AppendLine(
-                            $".Infix({realArg} {GetAttributeArgs(attribute, skip:1)},null)");
-                    }
-
-                    _builder.AppendLine($".Infix({GetAttributeArgs(attribute, withLeadingComma: false)},");
+                    var rule = GetAttributeArgs(attribute, withLeadingComma: false);
+                    _builder.AppendLine($".Operand({rule},");
                     AddProductionVisitor(node);
                     _builder.AppendLine(")");
                     break;
                 }
-                default:
+                case "Prduction" when !IsOperand(node):
                 {
+                    var rule = GetAttributeArgs(attribute, withLeadingComma: false);
+                    _builder.AppendLine($".Production({rule},");
+                    AddProductionVisitor(node);
+                    _builder.AppendLine(")");
+                    break;
+                }
+                case "Operation":
+                {
+                    AddOperation(node, attribute);
+                    break;
+                }
+                case "Prefix":
+                case "Postfix":
+                {
+                    AddPrePostFix(node, attribute);
+                    break;
+                }
+                case "Infix":
+                {
+                    AddInfix(node, attribute);
                     break;
                 }
             }
@@ -200,6 +130,58 @@ public class ParserSyntaxWalker : CslySyntaxWalker
         }
 
 
+    }
+
+    private void AddInfix(MethodDeclarationSyntax node, AttributeSyntax attribute)
+    {
+        if (attribute.ArgumentList.Arguments[0].Expression is CastExpressionSyntax cast)
+        {
+            var realArg = (attribute.ArgumentList.Arguments[0].Expression as CastExpressionSyntax).Expression.ToString();
+            _builder.AppendLine(
+                $".Infix({realArg} {GetAttributeArgs(attribute, skip:1)},null)");
+        }
+
+        _builder.AppendLine($".Infix({GetAttributeArgs(attribute, withLeadingComma: false)},");
+        AddProductionVisitor(node);
+        _builder.AppendLine(")");
+    }
+
+    private void AddPrePostFix(MethodDeclarationSyntax node, AttributeSyntax attribute)
+    {
+        var precedence = attribute.ArgumentList.Arguments[2].Expression.ToString();
+        if (attribute.ArgumentList.Arguments[0].Expression is CastExpressionSyntax cast)
+        {
+            var realArg = (attribute.ArgumentList.Arguments[0].Expression as CastExpressionSyntax).Expression.ToString();
+            _builder.AppendLine($".{attribute.Name.ToString()}({realArg}, {precedence}, ");
+            AddProductionVisitor(node);
+            _builder.AppendLine(")");
+        }
+        else
+        {
+            _builder.AppendLine(
+                $".{{attribute.Name.ToString()}}({attribute.ArgumentList.Arguments[0].Expression.ToString()}, {precedence},");
+            AddProductionVisitor(node);
+            _builder.AppendLine(")");
+        }
+    }
+
+    private void AddOperation(MethodDeclarationSyntax node, AttributeSyntax attribute)
+    {
+        if (attribute.ArgumentList.Arguments[0].Expression is CastExpressionSyntax cast)
+        {
+            var realArg = (attribute.ArgumentList.Arguments[0].Expression as CastExpressionSyntax).Expression.ToString();
+            _builder.AppendLine(
+                $".Operation({realArg} {GetAttributeArgs(attribute, skip:1)},");
+            AddProductionVisitor(node);
+            _builder.AppendLine(")");
+        }
+        else
+        {
+            _builder.AppendLine(
+                $".Operation({GetAttributeArgs(attribute, withLeadingComma: false)},");
+            AddProductionVisitor(node);
+            _builder.AppendLine(")");
+        }
     }
 
     private void AddProductionVisitor(MethodDeclarationSyntax method)
