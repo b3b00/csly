7a7fa4c0f8ebe46b660b01859a13a06082836d0d
b3b00 <olivier.duhart@gmail.com>
2025-01-17 09:08:44 +0100

    issue #527 unit test

--- tests/ParserTests/Issue527/Issue527Lexer.cs
+++ tests/ParserTests/Issue527/Issue527Lexer.cs
@@ -1,6 +1,13 @@
+using sly.lexer;
+
 namespace ParserTests.Issue527;
 
 public enum Issue527Lexer
 {
+    [Keyword("a")]
+    A = 1,
+    
+    [Keyword("b")]
+    B = 2,
     
 }
--- tests/ParserTests/Issue527/Issue527Test.cs
+++ tests/ParserTests/Issue527/Issue527Test.cs
@@ -1,6 +1,38 @@
+using NFluent;
+using sly.parser;
+using sly.parser.generator;
+using Xunit;
+
 namespace ParserTests.Issue527;
 
 public class Issue527Test
 {
+
+    private Parser<Issue527Lexer, string> BuildParser()
+    {
+        var builder = new ParserBuilder<Issue527Lexer, string>("en");
+        var result = builder.BuildParser(new Issue527Parser(), ParserType.EBNF_LL_RECURSIVE_DESCENT,"root");
+        Check.That(result).IsOk();
+        return result.Result;
+    }
     
+    [Fact]
+    public void Issue527Test_OK_with_ParseWithoutContext()
+    {
+        var parser = BuildParser();
+        var result = parser.Parse("a a b");
+        Check.That(result).IsOkParsing();
+        Check.That(result.Result).IsEqualTo("any(a,a) and boo(b)");
+    }
+    
+    [Fact]
+    public void Issue527Test_KO_with_ParseWithContext()
+    {
+        var parser = BuildParser();
+        var lexed = parser.Lexer.Tokenize("a a b");
+        Check.That(lexed).IsOkLexing();
+        var result = parser.ParseWithContext(lexed.Tokens.MainTokens());
+        Check.That(result).IsOkParsing();
+        Check.That(result.Result).IsEqualTo("any(a,a) and boo(b)");
+    }
 }
