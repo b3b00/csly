0bf17de62cd1b8f24bc241fb57265db106e1ec6d
b3b00 <olivier.duhart@gmail.com>
2025-03-25 14:05:34 +0100

    fix short char attribute -> v3.6.2

--- src/sly/lexer/attributes/CharAttribute.cs
+++ src/sly/lexer/attributes/CharAttribute.cs
@@ -5,7 +5,7 @@ namespace sly.lexer
     [AttributeUsage(AttributeTargets.Field, AllowMultiple = false, Inherited = false)]
     public class CharacterAttribute : LexemeAttribute
     {
-        public CharacterAttribute(string delimiter = "'", string escape = "\\") : base(GenericToken.String, delimiter, escape)
+        public CharacterAttribute(string delimiter = "'", string escape = "\\") : base(GenericToken.Char, delimiter, escape)
         {   
         } 
     }
