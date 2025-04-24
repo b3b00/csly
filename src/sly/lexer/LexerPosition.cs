477e49e5e12050584594c865ae77a7f9e2b52799
b3b00 <olivier.duhart@gmail.com>
2025-04-24 09:15:19 +0200

    remove newtonsoft dependency and replace with System.text.Json

--- src/sly/lexer/LexerPosition.cs
+++ src/sly/lexer/LexerPosition.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
-using Newtonsoft.Json;
+using System.Text.Json.Serialization;
 
 namespace sly.lexer
 
