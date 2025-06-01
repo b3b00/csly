6682c1aae823f3217c58ebfe3c071cf6203a16e1
b3b00 <olivier.duhart@gmail.com>
2025-06-01 09:06:32 +0200

    fix

--- src/sly/parser/parser/llparser/bnf/stackist/state/RootStackState.cs
+++ src/sly/parser/parser/llparser/bnf/stackist/state/RootStackState.cs
@@ -7,7 +7,7 @@ namespace sly.parser.llparser.bnf.stackist;
 [DebuggerDisplay("root")]
 public class RootStackState<IN,OUT> : StackState<IN,OUT> where IN : struct, Enum
 {
-    public override  StackStateType Type => StackStateType.Result;
+    public override  StackStateType Type => StackStateType.Root;
 
     public RootStackState() : base()
     {
