4e202fb8bd41fac7a9447ececa5d13ce1b6efa58
b3b00 <olivier.duhart@gmail.com>
2025-10-20 08:06:39 +0200

    fix unit test source

--- tests/ParserTests/Issue588/Issue588Tests.cs
+++ tests/ParserTests/Issue588/Issue588Tests.cs
@@ -14,12 +14,12 @@ public class Issue588Tests {
         var instance = new Parse();
         var build = builder.BuildParser(instance, ParserType.EBNF_LL_RECURSIVE_DESCENT, "root");
         Check.That(build).IsOk();
-        var source = @"IF x == 12
-    IF z == 78
+        var source = @"if x == 12
+    if z == 78
         w = 23
-        IF a == 46 
+        if a == 46 
             b = 93
-        ELSE    
+        else    
             b=39
     y = 14";
         var parsed = build.Result.Parse(source);
